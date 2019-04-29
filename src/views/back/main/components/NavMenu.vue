dca6f912fce14e19795d013ac3b443e8c1c0d755
y.liu <y.liu@17daxue.com>
2019-04-29 22:43:14 +0800

    commit：药品管理

--- src/views/back/main/components/NavMenu.vue
+++ src/views/back/main/components/NavMenu.vue
@@ -1,18 +1,18 @@
 <template>
-    <div>
-      <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" background-color="#545c64"
-               text-color="#fff"  active-text-color="#ffd04b" @select="handleSelect">
-        <template v-for="item in menus" v-show="!item.hidden" >
-          <el-menu-item v-if="!item.children" :index="item.index">{{ item.name }}</el-menu-item>
-          <el-submenu v-else-if="item.children" :index="item.index">
-            <template slot="title">{{ item.name }}</template>
-            <el-menu-item-group>
-              <el-menu-item v-for="(childMenu, i) in item.children" :key="i" :index="childMenu.index">{{ childMenu.name }}</el-menu-item>
-            </el-menu-item-group>
-          </el-submenu>
-        </template>
-      </el-menu>
-    </div>
+  <div>
+    <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" background-color="#545c64"
+             text-color="#fff"  active-text-color="#ffd04b" @select="handleSelect">
+      <template v-for="item in menus" v-show="!item.hidden" >
+        <el-menu-item v-if="!item.children" :index="item.index">{{ item.name }}</el-menu-item>
+        <el-submenu v-else-if="item.children" :index="item.index">
+          <template slot="title">{{ item.name }}</template>
+          <el-menu-item-group>
+            <el-menu-item v-for="(childMenu, i) in item.children" :key="i" :index="childMenu.index">{{ childMenu.name }}</el-menu-item>
+          </el-menu-item-group>
+        </el-submenu>
+      </template>
+    </el-menu>
+  </div>
 </template>
 
 <script>
