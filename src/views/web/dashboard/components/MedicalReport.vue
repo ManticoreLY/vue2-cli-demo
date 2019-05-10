dec5d12c0b11cde2f7af5563be72c76265d56a51
y.liu <y.liu@17daxue.com>
2019-05-10 09:49:49 +0800

    commit:

--- src/views/web/dashboard/components/MedicalReport.vue
+++ src/views/web/dashboard/components/MedicalReport.vue
@@ -3,14 +3,14 @@
     <div v-for="item in 3" :key="item" class="news-module">
       <div class="module-name">医疗公告</div>
       <div class="module-cont">
-        <div><img width="175" height="175"></div>
-        <div style="width: 190px">
+        <div style="width: 40%"><img width="160" height="160"></div>
+        <div style="width: 56%">
           <p class="ellipsis" style="font-size:2rem;color:#1daca4;height: 40px;">杨博士出席人工智能大会</p>
-          <p style="font-size: 1.75rem;color:#7f7f7f;text-indent: 3rem;width:190px;height: 100px;word-wrap: break-word">1月12日，南京大学人工智能生物医疗技术研究院落地江北地区签约仪式在汉府饭店举行<a>【详情】</a></p>
+          <p style="font-size: 1.5rem;color:#7f7f7f;text-indent: 3rem;width:190px;height: 100px;word-wrap: break-word">1月12日，南京大学人工智能生物医疗技术研究院落地江北地区签约仪式在汉府饭店举行<a>【详情】</a></p>
         </div>
       </div>
       <div class="module-title">
-        <div v-for="k in 6" :key="k" style="padding-left: 20px;margin-top:10px;">
+        <div v-for="k in 6" :key="k" style="margin-top:10px;">
           <span class="ellipsis" style="font-size: 1.25rem;color:#7f7f7f">XXXXXXXXXXXXXX</span>
           &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
           <span>2019-05-01</span>
@@ -28,10 +28,12 @@
 
 <style scoped>
   .ellipsis{white-space:nowrap;overflow: hidden;text-overflow: ellipsis}
-  .web-news{width: 90%;margin: 0 5%;height: auto;display: flex;flex-flow:row wrap;justify-content: space-around}
+  .web-news{width: 90%;margin: 0 5%;height: auto;display: flex;flex-flow:row wrap;justify-content: space-between}
   .web-news .news-module{position:relative;display: inline-block;width: 31%;margin-bottom: 20px;border-right: 1px solid #ccc}
   .web-news .news-module:last-child{border:none}
   .web-news .news-module .module-name{font-size: 2.25rem;height: 36px}
-  .web-news .news-module .module-cont{height: 175px}
+  .web-news .news-module .module-cont{height: 175px;width: 100%;display: inline-flex;flex-flow: row nowrap;justify-content: space-between;align-content: stretch;align-items: flex-start}
   .web-news .news-module .module-cont>div{display: inline-block;}
+  .web-news .news-module .module-title{width: 96%}
+  .web-news .news-module .module-title>div{display: flex; flex-flow: row nowrap; justify-content: space-between; align-items: center;align-content: stretch}
 </style>
