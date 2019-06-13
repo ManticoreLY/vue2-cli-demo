bdeb0e5c29e98997bf1082636e3e7600b6dc0a3f
DELL <594519665@qq.com>
2019-06-13 10:41:08 +0800

    首页相关代码

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,7 +1,7 @@
 const constant = {
   ImgUploadUrl: 'http://112.74.180.226/yinuo/uploading',
-  ServerUrl: 'http://localhost:8888/yinuo/'
-  // ServerUrl: 'http://112.74.180.226:8888/yinuo/'
+  // ServerUrl: 'http://localhost:8888/yinuo/'
+  ServerUrl: 'http://112.74.180.226:8888/yinuo/'
 }
 
 export default constant
